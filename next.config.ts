02f5fd5b4581c5b9e334ffbe1e242f2891c7b2d0
Harel Panker <harelpanker@gmail.com>
2024-12-09 09:27:11 +0100

    installing shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		dangerouslyAllowSVG: true,
+		contentDispositionType: 'attachment',
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'media.graphassets.com',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.graphcms.com',
+				pathname: '**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,33 +8,33 @@ export default {
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['var(--font-inter)'],
-				poppins: ['var(--font-poppins)'],
-				ibm: ['var(--font-ibm)'],
-			},
-			colors: {
-				transparent: 'transparent',
-				_FDF8F6: '#FDF8F6',
-				_451E11: '#451E11',
-				_974EF2: '#974EF2',
-				_FDFCFC: '#FDFCFC',
-				_2B1400: '#2B1400',
-				_A666F4: '#A666F4',
-				_FFF1E7: '#FFF1E7',
-				_A18072: '#A18072',
-				_FFCCA7: '#FFCCA7',
-				_D1B9B0: '#D1B9B0',
-				_7011E4: '#7011E4',
-				_FFE8D7: '#FFE8D7',
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)',
-			},
-		},
-	},
-	plugins: [],
+    	extend: {
+    		fontFamily: {
+    			inter: ['var(--font-inter)'],
+    			poppins: ['var(--font-poppins)'],
+    			ibm: ['var(--font-ibm)']
+    		},
+    		colors: {
+    			transparent: 'transparent',
+    			_FDF8F6: '#FDF8F6',
+    			_451E11: '#451E11',
+    			_974EF2: '#974EF2',
+    			_FDFCFC: '#FDFCFC',
+    			_2B1400: '#2B1400',
+    			_A666F4: '#A666F4',
+    			_FFF1E7: '#FFF1E7',
+    			_A18072: '#A18072',
+    			_FFCCA7: '#FFCCA7',
+    			_D1B9B0: '#D1B9B0',
+    			_7011E4: '#7011E4',
+    			_FFE8D7: '#FFE8D7'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
+    },
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
